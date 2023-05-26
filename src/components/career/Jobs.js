851bfce8a6564fe5ce6fe7d94ff66acde33758ff
UroojMurtaz <uroojmurtaza12@gmail.com>
2023-05-26 16:55:46 +0500

    Career

--- src/components/career/Jobs.js
+++ src/components/career/Jobs.js
@@ -1,5 +1,6 @@
+import { Button, Select } from "@mantine/core";
 import React from "react";
-
+import Dropdown from 'react-bootstrap/Dropdown';
 const jobs = [
   {
     title: "Data Scientist",
@@ -35,8 +36,8 @@ const jobs = [
 
 const JobPosting = ({ title, type, seats }) => {
   return (
-    <div className="w-full aspect-[1.2] flex flex-1 flex-col items-center justify-center gap-[10px] rounded-[24px] border-2 border-boxColor 2xl:py-[64px]">
-      <h1 className="font-semibold md:2xl:text-[37px] md:mt-0 sm:py-3">
+    <div className="w-full flex-1 aspect-[1.2] flex flex-col items-center justify-center gap-[10px] rounded-[24px] border-2 border-boxColor md:py-[30px]">
+      <h1 className="font-semibold sm:text-[30px] md:mt-0 sm:py-3">
         {title}
       </h1>
       <p className="2xl:text-[24px]">Type: {type}</p>
@@ -50,10 +51,59 @@ const JobPosting = ({ title, type, seats }) => {
 
 const Jobs = ({ title, type, seats }) => {
   return (
-    <div className="grid md:w-full mt-12 grid-cols-1 gap-x-[30px] gap-y-[36px] md:grid-cols-2 md:px-12 px-16 lg:grid-cols-3 2xl:h-[395px] 2xl:px-[130px]">
-      {jobs.map((job) => (
-        <JobPosting title={job.title} type={job.type} seats={job.seats} />
-      ))}
+    <div>
+
+      <div className="career-Filters">
+        <div class="job-heading">
+          <div class="jobs-title">
+            <span class="job-title">
+              {" "}
+              &lt;&nbsp;Job Filter&nbsp;&gt;{" "}
+            </span>
+            <h2 className='text-3xl font-bold'>Find your desire jobs and get recruited!</h2>
+          </div>
+        </div>
+
+
+
+
+
+        <div className="select-div">
+
+          <Select
+            data={['React', 'Angular', 'Svelte', 'Vue']}
+            placeholder="Pick one"
+            radius="md"
+            size="lg"
+          />
+          <Select
+            placeholder="Pick one"
+            data={['React', 'Angular', 'Svelte', 'Vue']}
+            radius="md"
+            size="lg"
+
+          />
+          <Select
+            data={['React', 'Angular', 'Svelte', 'Vue']}
+            placeholder="Pick one"
+            radius="md"
+            size="lg"
+          />
+
+          <button className="bg-white rounded-lg text-accentColor font-poppins font-semibold px-[52px] py-[10px]">
+            Filter
+          </button>
+
+        </div>
+
+      </div>
+
+
+      <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-20 w-[80%] m-auto my-20">
+        {jobs.map((job) => (
+          <JobPosting title={job.title} type={job.type} seats={job.seats} />
+        ))}
+      </div>
     </div>
   );
 };
--- src/pages/Career.js
+++ src/pages/Career.js
@@ -2,12 +2,15 @@ import React from "react";
 import HeroSection from "../components/career/HeroSection";
 import Jobs from "../components/career/Jobs";
 import CareerTitle from "../components/career/CareerTitle";
+import HiringProcess from "../components/career/HiringProcess";
 
 const Career = () => {
   return (
     <>
       <CareerTitle />
+      <HiringProcess />
       <HeroSection />
+     
       <Jobs />
     </>
   );
