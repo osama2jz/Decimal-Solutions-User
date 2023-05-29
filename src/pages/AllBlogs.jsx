86959d54789c6b91f5f480a02bdeb462dbc84035
Muhammad Usama <mosama4u@gmail.com>
2023-05-29 19:17:15 +0500

    contact us form

--- src/pages/AllBlogs.jsx
+++ src/pages/AllBlogs.jsx
@@ -1,149 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import pic from "../components/blogs/1111.avif";
 import blogImage from "../assets/images/BlogHero.png";
 import Card from "../components/blogs/Card";
 import Pagination from "../components/blogs/Pagination";
+import axios from "axios";
+import { backendUrl } from "../constants";
+import { Loader, Title } from "@mantine/core";
 
 const AllBlogs = () => {
-  const blogsArray = [
-    {
-      id: 1,
-      title: "Lorem 1",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-    {
-      id: 2,
-      title: "Lorem 2",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-    {
-      id: 2,
-      title: "Lorem 3",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = React.useState(0);
+  const [blogs, setBlogs] = useState([]);
 
-    {
-      id: 2,
-      title: "Lorem 4",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-    {
-      id: 2,
-      title: "Lorem 5",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-    {
-      id: 2,
-      title: "Lorem 6",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-    {
-      id: 2,
-      title: "Lorem 7",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-    {
-      id: 2,
-      title: "Lorem 8",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-    {
-      id: 2,
-      title: "Lorem 9",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-    {
-      id: 2,
-      title: "Lorem 10",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-
-    {
-      id: 2,
-      title: "Lorem 14",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-    {
-      id: 2,
-      title: "Lorem 135",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-    {
-      id: 2,
-      title: "Lorem 16",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-    {
-      id: 2,
-      title: "Lorem 16",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-    {
-      id: 2,
-      title: "Lorem 17",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-    {
-      id: 2,
-      title: "Lorem 18",
-      description:
-        "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin",
-      date: "23 December 2021",
-      image: blogImage,
-    },
-  ];
+  useEffect(() => {
+    setIsLoading(true);
+    axios.get(backendUrl + "/api/v1/web/blogs").then((res) => {
+      setBlogs(res.data.data);
+      setIsLoading(false);
+    });
+  }, []);
   const backgroundImage = {
     backgroundImage: `url("${blogImage}")`,
     backgroundSize: "cover",
     backgroundPosition: "center",
   };
-  const [blogs, setBlogs] = React.useState(blogsArray);
-  const [page, setPage] = React.useState(0);
   return (
     <div>
       <div
@@ -155,21 +35,33 @@ const AllBlogs = () => {
           OUR BLOG
         </h1>
       </div>
-      <div className="grid sm:grid-cols-1 cardBreak2:grid-cols-3 cardBreak3:grid-cols-3  justify-around px-28 pt-12">
-        {blogs.map((blog, index) => {
-          if (index >= page * 6 && index < page * 6 + 6) {
-            return (
-              <Card
-                key={index}
-                image={blog.image}
-                title={blog.title}
-                date={blog.date}
-                description={blog.description}
-              />
-            );
-          }
-        })}
-      </div>
+      {isLoading ? (
+        <Loader
+          size={"xl"}
+          color="purple"
+          style={{ margin: "auto", marginBlock: "100px" }}
+        />
+      ) : (
+        <div className="grid sm:grid-cols-1 cardBreak2:grid-cols-3 cardBreak3:grid-cols-3  justify-around px-28 pt-12">
+          {blogs.length ? (
+            blogs?.map((blog, index) => {
+              if (index >= page * 6 && index < page * 6 + 6) {
+                return (
+                  <Card
+                    key={index}
+                    image={blog.blogImage}
+                    title={blog.blogTitle}
+                    date={blog.blogDate}
+                    description={blog.blogDescription}
+                  />
+                );
+              }
+            })
+          ) : (
+            <Title>No Blog Found.</Title>
+          )}
+        </div>
+      )}
       <div className="p-16">
         <Pagination length={blogs?.length} setPage={setPage} page={page} />
       </div>
