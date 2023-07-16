e00fbfc03ac77f781ab6a62d684ef025be093d3e
Muhammad Talha <muhammadtalha517@gmail.com>
2023-07-17 01:07:35 +0500

    slider issue 40% fixed

--- src/components/home/Landing-Page/Services.js
+++ src/components/home/Landing-Page/Services.js
@@ -54,7 +54,7 @@ function Services() {
   }, []);
   var settings = {
     // dots: true,
-    infinite: false,
+    infinite: true,
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
@@ -67,7 +67,7 @@ function Services() {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
-          infinite: true,
+
           // dots: true,
         },
       },
@@ -76,6 +76,7 @@ function Services() {
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
+
           // infinite: true,
           // dots: true,
         },
@@ -83,16 +84,15 @@ function Services() {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 1,
+          slidesToShow: 1,
+          slidesToScroll: 1,
         },
       },
       {
         breakpoint: 480,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToScroll: 0,
         },
       },
     ],
@@ -109,19 +109,22 @@ function Services() {
       >
         <div className="heading">
           <div className="services-title">
-            <span className="ser-title"> &lt;&nbsp;Our Services&nbsp;&gt; </span>
+            <span className="ser-title">
+              {" "}
+              &lt;&nbsp;Our Services&nbsp;&gt;{" "}
+            </span>
             <h2 className="font-bold text-xl">View some of our services</h2>
           </div>
         </div>
         <Slider {...settings}>
           {services.map((obj, index) => {
             return (
-              <div style={{ marginRight: "3rem" }} key={index}>
+              <div key={index}>
                 <div className="carousel__slide">
                   <div className="card">
                     <div className="card__inner">
                       <div className="numbering-inside">
-                        <p className="text">{index+1}</p>
+                        <p className="text">{index + 1}</p>
                       </div>
                       <div className="card__image">
                         <img src={obj?.homeImage} className="img" />
